LineageOS/android_device_lineage_sepolicy

qcom: Add legacymm livedisplay sepolicy
Change-Id: Ic8e243e18cb9acf842d7afd9af0cecbf4550b1ae

qcom/sepolicy.mk
@@ -21,3 +21,8 @@ BOARD_SEPOLICY_DIRS += \
     device/lineage/sepolicy/qcom/dynamic \
     device/lineage/sepolicy/qcom/vendor
 endif
+
+ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/qcom/legacy-vendor
+endif

Zeus-OS/android_device_zeus_sepolicy

qcom/sepolicy.mk
@@ -21,3 +21,8 @@ BOARD_SEPOLICY_DIRS += \
     device/zenx/sepolicy/qcom/dynamic \
     device/zenx/sepolicy/qcom/vendor
 endif
+
+ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+BOARD_SEPOLICY_DIRS += \
+    device/zenx/sepolicy/qcom/legacy-vendor
+endif